a2a87ae9f411efee1a99f1c352f627f27afd0919
Bianca Boccato <biancab@motorola.com>
2019-03-02 21:21:13 -0300

    Step 3 complete

--- backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/udacity/gradle/builditbigger/backend/MyEndpoint.java
@@ -21,10 +21,10 @@ public class MyEndpoint {
 
     /** A simple endpoint method that takes a name and says Hi back */
     @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
+    public MyBean sayHi() {
         MyBean response = new MyBean();
         JavaJoker joker = new JavaJoker();
-        response.setData("Hi, " + name + joker.getJoke());
+        response.setData(joker.getJoke());
 
         return response;
     }
